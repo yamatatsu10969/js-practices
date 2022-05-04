4c282bf04d8d5b050f1138eb082e60ac6ee3cf84
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-04 18:31:11 +0900

    sqlite3 を使用して memo の CRUD を作成

---
+++ 04.class/lib/memo-database-client.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+class MemoDatabaseClient {
+  constructor () {
+    const sqlite3 = require('sqlite3')
+    this.db = new sqlite3.Database('../memos.db')
+  }
+
+  async initialize () {
+    return new Promise(resolve => {
+      this.db.run(`
+      CREATE TABLE IF NOT EXISTS memos(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      body TEXT NOT NULL)`,
+      (_result, _error) => resolve())
+    })
+  }
+
+  async all () {
+    console.log('all')
+    return new Promise(resolve =>
+      this.db.all('SELECT * FROM memos', (_, rows) => {
+        return resolve(rows)
+      })
+    )
+  }
+
+  async save (body) {
+    console.log('save')
+    return new Promise(resolve =>
+      this.db.run(
+        'INSERT INTO memos(body) values(?)',
+        body,
+        (_result, _err) => resolve()
+      )
+    )
+  }
+
+  async delete (id) {
+    console.log('delete')
+    this.db.run('DELETE FROM memos WHERE id = ?', id)
+  }
+
+  // TODO: 後で消す
+  async dropTableForTesting () {
+    return new Promise(resolve => {
+      this.db.run('DROP TABLE IF EXISTS memos',
+        (_result, _error) => resolve())
+    })
+  }
+
+  dispose () {
+    this.db.close()
+  }
+}
+
+const memoDatabaseClient = new MemoDatabaseClient()
+
+module.exports.memoDatabaseClient = memoDatabaseClient
