6112ba57b37a87f8587954ba9a4d1db158441176
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-07 17:04:00 +0900

    print する文をキャメルケースに変更

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,11 +2,11 @@
 
 for (let i = 1; i < 21; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log('fizzbuzz');
+    console.log('FizzBuzz');
   } else if (i % 3 === 0) {
-    console.log('fizz');
+    console.log('Fizz');
   } else if (i % 5 === 0) {
-    console.log('buzz');
+    console.log('Buzz');
   } else {
     console.log(i);
   }
