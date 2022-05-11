2151d52c8aa6dc447eefb5eecf6321e1f1b87149
Yamamoto Tatsuya <43805056+yamatatsu10969@users.noreply.github.com>
2022-05-11 09:55:06 +0900

    ファイル操作は不要なので削除

--- 04.class/lib/memo-database-client.js
+++ 04.class/lib/memo-database-client.js
@@ -3,9 +3,7 @@
 class MemoDatabaseClient {
   constructor () {
     const sqlite3 = require('sqlite3')
-    const fs = require('fs')
-    fs.existsSync('../memo.db') || fs.writeFileSync('../memo.db', '')
-    this.db = new sqlite3.Database('../memo.db')
+    this.db = new sqlite3.Database('./memo.db')
   }
 
   async initialize () {
